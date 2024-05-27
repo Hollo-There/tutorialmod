9d224301fea954f42ad77a6cd2f4cdf16db62187
Hollo-There <hmf.atx@gmail.com>
2024-05-27 13:36:26 -0500

    New Creative Tab added Two Items added: 128 brick from 2048 sapphire

---
+++ src/main/java/net/H0lll0W/tutorialmod/item/ModCreativeModeTabs.java
@@ -0,0 +1,32 @@
+package net.H0lll0W.tutorialmod.item;
+
+import net.H0lll0W.tutorialmod.TutorialMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> RANDOM_TAB = CREATIVE_MODE_TABS.register("random_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.BRICK2048_128.get()))
+                    .title(Component.translatable("creativetab.random_tab"))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(ModItems.SAPPHIRE.get());
+                        pOutput.accept(ModItems.BRICK2048_128.get());
+                        pOutput.accept(ModItems.BRICK2048_128.get());
+
+
+                    })
+                    .build());
+
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
---
+++ src/main/java/net/H0lll0W/tutorialmod/item/ModItems.java
@@ -0,0 +1,22 @@
+package net.H0lll0W.tutorialmod.item;
+
+import net.H0lll0W.tutorialmod.TutorialMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> BRICK2048_128 = ITEMS.register("brick2048_128",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
